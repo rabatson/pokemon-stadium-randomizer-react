b81a233a83b9aa5416f2fb809ea3d07a9ad840c2
Riley <59605363+rabatson@users.noreply.github.com>
2025-02-19 10:00:15 -0600

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import favicon from '/favicon.svg'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={favicon} className="logo" alt="Vite logo" />
+        </a>
+      <h1>Random Pokémon Team Generator for Pokémon Stadium</h1>
+      <p>Project Setup Placeholder Page</p>
+      </div>
+  )
+}
+
+export default App
